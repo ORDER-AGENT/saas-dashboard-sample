492eeaeda3d9b1319fe8683e05037aafa469569e
ORDER-AGENT <info.orderagent@gmail.com>
2025-07-24 23:18:39 +0900

    button を Button に変更

--- src/components/CardHeader.tsx
+++ src/components/CardHeader.tsx
@@ -8,6 +8,7 @@ import {
   DropdownMenuItem,
   DropdownMenuSeparator,
 } from '@/components/ui/dropdown-menu';
+import { Button } from '@/components/ui/button';
 
 interface CardHeaderProps {
   title: string;
@@ -20,9 +21,9 @@ const CardHeader: React.FC<CardHeaderProps> = ({ title, dropdownItems }) => {
       <h3 className="text-xl font-semibold">{title}</h3>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-            <button className="text-gray-500 hover:text-gray-700">
+            <Button variant="ghost" size="icon">
                 <HiOutlineDotsHorizontal className="w-5 h-5" />
-            </button>
+            </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent className="w-56" align="center"> {/* ドロップダウンのコンテンツ */}
           {dropdownItems && dropdownItems.length > 0 ? (
